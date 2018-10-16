8e4d6c71ffb2c4dff8fd4371aed5b203af3cbdfb
Jasmin Johal <jjohal52@gmail.com>
2018-10-15 19:31:50 -0700

    Added hovertext for bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -28,6 +28,7 @@ function buildCharts(sample) {
     var trace1 = {
       x: data.otu_ids,
       y: data.sample_values,
+      hovertext: data.otu_labels,
       mode: 'markers',
       marker: {
         size: data.sample_values,
@@ -38,14 +39,7 @@ function buildCharts(sample) {
 
     var bubble_data = [trace1];
 
-    var layout = {
-      title: 'Marker Size'
-      // showlegend: false,
-      // height: 600,
-      // width: 600
-    };
-
-    Plotly.newPlot("bubble", bubble_data, layout);
+    Plotly.newPlot("bubble", bubble_data);
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
@@ -60,12 +54,71 @@ function buildCharts(sample) {
 
     var pie_data = [trace2];
 
-    var layout = {
-      title: "Pie Chart"
-    };
-
-    Plotly.newPlot("pie", pie_data, layout);
+    Plotly.newPlot("pie", pie_data);
   });
+
+// // Enter a speed between 0 and 180
+// var level = 175;
+
+// // Trig to calc meter point
+// var degrees = 180 - level,
+//      radius = .5;
+// var radians = degrees * Math.PI / 180;
+// var x = radius * Math.cos(radians);
+// var y = radius * Math.sin(radians);
+
+// // Path: may have to change to create a better triangle
+// var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+//      pathX = String(x),
+//      space = ' ',
+//      pathY = String(y),
+//      pathEnd = ' Z';
+// var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+// var gauge_data = [{ type: 'scatter',
+//    x: [0], y:[0],
+//     marker: {size: 28, color:'850000'},
+//     showlegend: false,
+//     name: 'speed',
+//     text: level,
+//     hoverinfo: 'text+name'},
+//   { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
+//   rotation: 90,
+//   text: ['TOO FAST!', 'Pretty Fast', 'Fast', 'Average',
+//             'Slow', 'Super Slow', ''],
+//   textinfo: 'text',
+//   textposition:'inside',
+//   marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+//                          'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+//                          'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+//                          'rgba(255, 255, 255, 0)']},
+//   labels: ['151-180', '121-150', '91-120', '61-90', '31-60', '0-30', ''],
+//   hoverinfo: 'label',
+//   hole: .5,
+//   type: 'pie',
+//   showlegend: false
+// }];
+
+// var layout = {
+//   shapes:[{
+//       type: 'path',
+//       path: path,
+//       fillcolor: '850000',
+//       line: {
+//         color: '850000'
+//       }
+//     }],
+//   title: 'Gauge 
+//  Speed 0-100',
+//   height: 1000,
+//   width: 1000,
+//   xaxis: {zeroline:false, showticklabels:false,
+//              showgrid: false, range: [-1, 1]},
+//   yaxis: {zeroline:false, showticklabels:false,
+//              showgrid: false, range: [-1, 1]}
+// };
+
+// Plotly.newPlot('gauge', gauge_data, layout);
 }
 
 function init() {
